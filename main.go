44089aafd6a32c30fc7564c6eeb2a9a6885811a7
Taylor Wrobel <taylor.wrobel@gmail.com>
2019-09-21 14:37:05 -0700

    Switch to algorithm by @AustinTSchaffer

--- main.go
+++ main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"encoding/csv"
 	"fmt"
 	"math/big"
 	"os"
 	"strconv"
-	"strings"
 )
 
 func main() {
@@ -20,29 +18,11 @@ func main() {
 		n = int(parsed)
 	}
 
-	// Likelihoods is a slice that's built up iteratively, as the expected value
-	// on lillypad n is dependent on the expected value for all lillypads from
-	// 1 to n-1.
-	likelihoods := make([]*big.Rat, n+1)
-	likelihoods[0] = big.NewRat(0, 1)
-
-	w := csv.NewWriter(os.Stdout)
+	sum := big.NewRat(0, 1)
 
 	for i := 1; i <= n; i++ {
-		expected := big.NewRat(0, 1)
-		for j := 0; j < i; j++ {
-			value := big.NewRat(0, 1)
-			value.Add(big.NewRat(1, 1), likelihoods[j])
-			value.Mul(value, big.NewRat(1, int64(i)))
-
-			expected.Add(expected, value)
-		}
-		likelihoods[i] = expected
-
-		numDenom := strings.Split(expected.String(), "/")
-		f, _ := expected.Float64()
-		w.Write([]string{fmt.Sprintf("%d", i), numDenom[0], numDenom[1], fmt.Sprintf("%f", f)})
+		sum = sum.Add(sum, big.NewRat(1, int64(i)))
 	}
 
-	w.Flush()
+	fmt.Printf("Frog(%d) = %s == %s\n", n, sum.String(), sum.FloatString(10))
 }
